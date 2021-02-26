7cc1c2e2056cfbfee52fe04d5a5092037bd700c5
Dina AbdelRahman <36127536+dinaAbdelrahman@users.noreply.github.com>
2021-02-26 17:07:41 +0400

    Add files via upload

--- train.py
+++ train.py
@@ -70,5 +70,8 @@ def main():
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
 
+    os.makedirs('outputs',exist_ok=True)
+    joblib.dump(value=model, filename='outputs/model.pkl')
+
 if __name__ == '__main__':
     main()
